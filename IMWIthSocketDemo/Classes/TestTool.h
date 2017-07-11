cc8b133f9bc1671ca96898fba48bb8a81afd96b7
guchunli <1912029196@qq.com>
2017-07-11 14:32:51 +0800

    spec test

---
+++ IMWIthSocketDemo/Classes/TestTool.h
@@ -0,0 +1,13 @@
+//
+//  TestTool.h
+//  IMWIthSocketDemo
+//
+//  Created by cheyifu on 2017/7/11.
+//  Copyright © 2017年 guchunli. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestTool : NSObject
+
+@end
---
+++ IMWIthSocketDemo/Classes/TestTool.m
@@ -0,0 +1,13 @@
+//
+//  TestTool.m
+//  IMWIthSocketDemo
+//
+//  Created by cheyifu on 2017/7/11.
+//  Copyright © 2017年 guchunli. All rights reserved.
+//
+
+#import "TestTool.h"
+
+@implementation TestTool
+
+@end
